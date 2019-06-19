51af0fdb1f754f903b2c214c70e02e222a5e463c
Macbook Pro <macbook-pro@MacBook-Pro-de-Macbook.local>
2019-06-19 17:18:57 +0200

    experimenting mutex

--- prime_threadsafe.c
+++ prime_threadsafe.c
@@ -1,30 +1,34 @@
 #include "threads.h"
 #define NB_THREAD 4
 
-int i;
-int count;
+atomic_t i;
+atomic_t count;
 
 void print_if_prime(int a)
 {
 	if (a == 2)
 	{
 		printf("%d\n", a);
-		count++;
+		pthread_mutex_lock(&count.mutex);
+		count.var++;
+		pthread_mutex_unlock(&count.mutex);
 		return;
 	}
 	for(int k = 2; k < a; k++)
 		if (a % k == 0) return;
 	printf("%d\n", a);
-	count++;
+	count.var++;
 }
 
 void *print_primes(void *arg)
 {
 	int a = 0;
 	int max = (int) arg;
-	while (count <= max)
+	while (count.var < max)
 	{
-		a = ++i;
+		pthread_mutex_lock(&i.mutex);
+		a = ++i.var;
+		pthread_mutex_unlock(&i.mutex);
 		print_if_prime(a);
 	}
 	pthread_exit(NULL);
@@ -35,12 +39,14 @@ int main(int argc, char** argv)
 	int			max = 100;
 	pthread_t	threads[NB_THREAD];
 	if (argc == 2) max = atoi(argv[1]);
-	i = 1;
-	count = 0;
+	i.var = 1;
+	count.var = 0;
+	pthread_mutex_init(&i.mutex, NULL);
+	pthread_mutex_init(&count.mutex, NULL);
 	for (int k = 0; k < NB_THREAD; k++)
 		pthread_create (&threads[k], NULL, print_primes, (void*) max);
 	for (int k = 0; k < NB_THREAD; k++)
 		pthread_join(threads[k], NULL);
-	printf("printed %d primes\n", count);
+	printf("printed %d primes\n", count.var);
 	return 0;
 }
